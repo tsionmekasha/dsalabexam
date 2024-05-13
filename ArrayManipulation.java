519496b3b95078be9e2887d949716c084968cc8f
tsionmekasha <167539440+tsionmekasha@users.noreply.github.com>
2024-05-13 06:03:12 -0700

    Update ArrayManipulation.java

--- ArrayManipulation.java
+++ ArrayManipulation.java
@@ -1,19 +1,38 @@
-/**
- * 
- */
 package question6;
 
-/**
- * 
- */
+
 public class ArrayManipulation {
+    public static void main(String[] args) {
+        int[] array = {3, 7, 1, 9, 4};
+        int indexToDelete = 2; 
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        int[] modifiedArray = deleteElement(array, indexToDelete);
 
-	}
+        if (modifiedArray != null) {
+            System.out.println("Modified Array:");
+            for (int num : modifiedArray) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        } else {
+            System.out.println("Invalid index. Original array remains unchanged.");
+        }
+    }
 
+    public static int[] deleteElement(int[] array, int index) {
+        if (index < 0 || index >= array.length) {
+            System.out.println("Invalid index.");
+            return array; 
+        }
+
+        int[] modifiedArray = new int[array.length - 1]; 
+        int j = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (i != index) { 
+                modifiedArray[j++] = array[i]; 
+            }
+        }
+
+        return modifiedArray; 
+    }
 }
